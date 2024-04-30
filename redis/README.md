1752b0e0004015fc83b2f0352ff365b4fe4c7984
tp_chenghao <406434543@qq.com>
2024-04-30 14:03:49 +0800

    redisson

---
+++ redis/README.md
@@ -0,0 +1,3 @@
+# redisson
+* [GitHub](https://github.com/redisson/redisson)
+* [redisson-spring-boot-starter](https://github.com/redisson/redisson/tree/master/redisson-spring-boot-starter)
--- redis/src/test/java/com/example/redis/RedisApplicationTests.java
+++ redis/src/test/java/com/example/redis/RedisApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.redis;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedisApplicationTests {
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     @Test
     void contextLoads() {
+
+        RBucket<String> test = redissonClient.getBucket("key");
+
+        System.out.println(test.get());
     }
 
 }
