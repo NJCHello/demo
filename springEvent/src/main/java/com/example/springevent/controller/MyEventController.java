996ddff48677b16c898599877181a3dc37dafb59
chello0o <406434543@qq.com>
2024-05-30 15:16:31 +0800

    Aspect

--- springEvent/src/main/java/com/example/springevent/controller/MyEventController.java
+++ springEvent/src/main/java/com/example/springevent/controller/MyEventController.java
@@ -1,5 +1,6 @@
 package com.example.springevent.controller;
 
+import com.example.springevent.config.ThreadPoolConfig;
 import com.example.springevent.event.MyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +20,16 @@ public class MyEventController {
     @Resource
     private ApplicationContext applicationContext;
 
+    @Resource
+    private ThreadPoolConfig threadPoolConfig;
+
     /**
      * http://localhost:8080/myEvent/publishEvent?id=11112
      * @param id
      */
     @GetMapping("/publishEvent")
     public void publishEvent(String id) {
+        System.out.println(threadPoolConfig.getMaxPoolSize());
         applicationContext.publishEvent(new MyEvent(id));
     }
 }
