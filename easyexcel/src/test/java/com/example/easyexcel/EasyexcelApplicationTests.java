8755e5c30c73996a22d7b0467250299d5002b26b
chello0o <406434543@qq.com>
2024-07-16 17:10:00 +0800

    ClassPathResource

--- easyexcel/src/test/java/com/example/easyexcel/EasyexcelApplicationTests.java
+++ easyexcel/src/test/java/com/example/easyexcel/EasyexcelApplicationTests.java
@@ -8,6 +8,17 @@ class EasyexcelApplicationTests {
 
     @Test
     void contextLoads() {
+        try {
+            //ClassPathResource classPathResource  = new ClassPathResource("template/XXXXXX.xlsx");
+            //String fileName = Test.class.getResource("/").getPath() + "template" + File.separator + "XXXXXX.xlsx";
+            // 这里 只要，然后读取第一个sheet 同步读取会自动finish
+            //EasyExcel.read(classPathResource.getInputStream(), new DemoDataListener(xxMapper,xxMapper,xxMapper,xxMapper)).sheet(0).doRead();
+            //EasyExcel.read(classPathResource.getInputStream(), new DemoDataListener(xxMapper,xxMapper,xxMapper,xxMapper)).sheet(2).doRead();
+            //EasyExcel.read(classPathResource.getInputStream(), new DemoDataListener(xxMapper,xxMapper,xxMapper,xxMapper)).sheet(3).doRead();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
