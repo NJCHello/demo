79a9709d5fe5a59befdb45b71e0b7471a50af9b6
chello0o <406434543@qq.com>
2024-07-29 17:25:00 +0800

    Class.forName

--- spi/src/main/java/com/example/spi/service/SerializerTest.java
+++ spi/src/main/java/com/example/spi/service/SerializerTest.java
@@ -1,13 +1,24 @@
 package com.example.spi.service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author chenghao
  * @since 2024/5/30 10:18
  */
 public class SerializerTest {
 
-    public static void main(String[] args) {
-        String serializeStr = Serializer.load().serialize(new Object());
-        System.out.println(serializeStr);
+//    public static void main(String[] args) {
+//        String serializeStr = Serializer.load().serialize(new Object());
+//        System.out.println(serializeStr);
+//    }
+
+    public static void main(String[] args) throws Exception {
+        Class<?> clazz = Class.forName("com.example.spi.service.SerializerImpl2");
+        Object instance = clazz.newInstance();
+        Method method = clazz.getMethod("serialize",Object.class);
+        String binder = (String) method.invoke(instance, new Object());
+        System.out.println(binder);
     }
 }
