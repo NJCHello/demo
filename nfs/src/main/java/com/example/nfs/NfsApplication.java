a5101a300137ac5330a1cf3a3bfaf8254540d491
chello0o <406434543@qq.com>
2024-08-05 11:53:57 +0800

    nfs

---
+++ nfs/src/main/java/com/example/nfs/NfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.nfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfsApplication.class, args);
+    }
+
+}
---
+++ nfs/src/test/java/com/example/nfs/NfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
